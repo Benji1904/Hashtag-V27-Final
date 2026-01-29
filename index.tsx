12a86b28f59edc489d41ac4e6118b5addda39856
Benji1904 <benjibikamwa@gmail.com>
2026-01-29 10:08:58 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './services/firebase'; // Side-effect: Initialize Firebase
-import { AuthProvider } from './context/AuthContext';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
